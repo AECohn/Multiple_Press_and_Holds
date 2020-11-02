5f9fc77e1a12fe2cbe789a0775a366f9fdb8eefe
Aviv <acohn@crestron.com>
2020-11-02 12:29:29 -0500

    init

---
+++ Class1.cs
@@ -0,0 +1,56 @@
+﻿//Programmer: Aviv Cohn
+
+using System;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace Multiple_Press_and_Holds
+{
+
+    public class MultipleHold
+    {
+
+        public delegate void ReportIndex(ushort index);
+        public ReportIndex SendIndex { get; set; }
+
+        private ushort Num_Holds = 76;
+        public ushort Hold_Time;
+        public ushort[] Hold_Values;
+        private bool[] Status;
+        public CTimer[] Holds;
+
+        public MultipleHold()
+        {
+            Hold_Values = new ushort[Num_Holds];
+            Status = new bool[Num_Holds];
+            Holds = new CTimer[Num_Holds];
+        }
+
+        public void Press(ushort ArrayIndex)
+        {
+            Status[ArrayIndex] = false;
+            Holds[ArrayIndex] = new CTimer(Hold_Time_Elapsed, ArrayIndex, Hold_Time);
+        }
+
+        public void Letgo(ushort ArrayIndex)
+        {
+            if (Status[ArrayIndex] == false)
+            {
+                Holds[ArrayIndex].Dispose();
+                Hold_Values[ArrayIndex] = 0;
+
+                SendIndex(ArrayIndex);
+            }
+        }
+
+        private void Hold_Time_Elapsed(object obj)
+        {
+            ushort ArrayIndex = (ushort)obj;
+            Status[ArrayIndex] = true;
+            Hold_Values[ArrayIndex] = 1;
+
+            SendIndex((ushort)obj);
+        }
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Multiple_Press_and_Holds")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Multiple_Press_and_Holds")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
