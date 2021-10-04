1c138f4450b24216e0cd167cb2570cfcee2bf442
AECohn <34989255+AECohn@users.noreply.github.com>
2021-10-04 11:36:40 -0400

    moved to new solution

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyTitle("Multiple_Press_and_Holds")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Multiple_Press_and_Holds")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyVersion("1.0.0.*")]
 
